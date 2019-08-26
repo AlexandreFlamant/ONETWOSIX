96c1e2844c60e1496c0822414a0679c3ff681d04
Ellen Lee <ellen811003@gmail.com>
2019-08-26 17:06:54 +0100

    skip devise for home and index pages also added name in the view devise registration

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :combos, only: [:index, :create]
+  resources :combos, only: :index
   resource :profile, only: :show
 end
