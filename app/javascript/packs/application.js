da2ed62e1b06b625112185e796896ce6a33d327a
Evelina Larsson <aevelinalarsson@gmail.com>
2019-08-27 13:55:37 +0100

    carousel and modal created, styling needed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,19 @@
 import "bootstrap";
+import $ from "jquery";
+import slick from "../../assets/slick/slick.min.js";
+
+
+$(document).ready(function(){
+
+  $('.carousel').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+  })
+
+  $('.modal').on('shown.bs.modal', function () {
+    $('.carousel').slick("setPosition", 0);
+  })
+});
+
+
