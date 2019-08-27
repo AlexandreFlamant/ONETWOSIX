c4ba4e4eee51cd24f780df4f7ada2f6c4208ed9d
Ellen Lee <ellen811003@gmail.com>
2019-08-27 13:03:49 +0100

    added address to restaurants table and added seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_26_152120) do
+ActiveRecord::Schema.define(version: 2019_08_27_113015) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2019_08_26_152120) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "link_url"
+    t.string "address"
   end
 
   create_table "users", force: :cascade do |t|
