46b9d3ca10fc8185c775ff32d359781da50fb612
Ellen Lee <ellen811003@gmail.com>
2019-08-28 13:57:36 +0100

    avoid user click on 2 cards

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -25,17 +25,28 @@ initAutocomplete();
 // for genre and foodtype selection in carousel
 $(document).ready(function(){
   $(".select-choice").click(function(){
+    $(this).closest('.check_boxes').find('.select-choice').removeClass('selected');
+    $(this).closest('.check_boxes').find(".fa-check-circle").removeClass("show");
     $(this).toggleClass("selected");
     $(this).find(".fa-check-circle").toggleClass("show");
   });
 });
 
-$(":checkbox").click(function(){
-  $length = $(":checkbox").length();
-  if ( $length > 2 ) {
-    $(":checkbox:checked").attr("disabled", true);
-  } else {
-    $(":checkbox").removeAttr("disabled");
-  }
-});
 
+
+// $(":checkbox").click(function(){
+//   $length = $(":checkbox").length();
+//   if ( $length > 2 ) {
+//     $(":checkbox:checked").attr("disabled", true);
+//   } else {
+//     $(":checkbox").removeAttr("disabled");
+//   }
+// });
+
+
+
+  // $('input[type=checkbox]').change(function(){
+  //    if ($('input[type=checkbox]:checked').length > 1) {
+  //         $(this)..attr('checked', true);
+  //    }
+  // })
