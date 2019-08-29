6db5617027ae09f387db6236fcb26db0ddd09c1a
Alexandre Flamant <alexflamant@gmail.com>
2019-08-29 14:01:09 +0100

    metadata added, missing heroku push

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+  def default_url_options
+    { host: ENV["https://www.onetwosix.co.uk/"] || "localhost:3000" }
+  end
 end
