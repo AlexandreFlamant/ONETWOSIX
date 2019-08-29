7d4d8e644aecd99bf00d820c29fa124767a3bdfa
Ellen Lee <ellen811003@gmail.com>
2019-08-29 21:32:52 +0100

    added name into application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,13 @@ class ApplicationController < ActionController::Base
   before_action :store_user_location!, if: :storable_location?
   before_action :authenticate_user!
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
   def after_sign_in_path_for(resource)
     stored_location_for(resource) || root_path
   end
